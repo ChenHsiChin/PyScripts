c23b3b292b1501d5258dd3cc8fa6a499d39280b8
Oops404 <cheneyjin@outlook.com>
2019-09-23 15:21:01 +0800

    Update pip-ali.bat

--- pip-ali.bat
+++ pip-ali.bat
@@ -1,6 +1,6 @@
 rem cheneyjin@outlook.com
 @set /p input= pip install what? 
-set user_input=%input%
+set user_input_lib=%input%
 set ali_mirrors= -i https://mirrors.aliyun.com/pypi/simple
-set command=%user_input%%ali_mirrors%
+set command=pip install %user_input_lib%%ali_mirrors%
 call %%command%%
