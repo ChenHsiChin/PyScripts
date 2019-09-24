bd166596296e6facf9e157d868e797c80a558f61
Oops404 <cheneyjin@outlook.com>
2019-09-24 15:13:57 +0800

    refactoring
    add bat fun

--- pip-ali.bat
+++ pip-ali.bat
@@ -1,6 +1,14 @@
 rem cheneyjin@outlook.com
+@echo off
 @set /p input= pip install what? 
-set user_input_lib=%input%
-set ali_mirrors= -i https://mirrors.aliyun.com/pypi/simple
-set command=pip install %user_input_lib%%ali_mirrors%
-call %%command%%
+if not "%input%"=="" ( call:downloadPythonLibs %input% )
+goto:finish
+
+:downloadPythonLibs
+set user_input_lib=%1
+set ali_mirrors=-i https://mirrors.aliyun.com/pypi/simple
+set command=pip install %user_input_lib% %ali_mirrors%
+call %command%
+goto:eof
+
+:finish
